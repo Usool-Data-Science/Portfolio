9463bb1c38b9fa09799ec79e8b162ea3de998ca0
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-06-24 17:27:01 +0100

    update

--- static/JS/new.js
+++ static/JS/new.js
@@ -1,60 +1,35 @@
 $(document).ready(function () {
-    var $title = $(".contents h2 span");
-    var $all_nav = $(".bottomContainer .navigator .navs .nav");
-    var $tabs = $(".bottomContainer .contents #nav2 .panel-tabs .tab");
-    var $linkers = $(".portalContainer .frame .linker").children();
-    var $blogLinkers = $(".blogContainer .blogFrame .linker").children();
-    var $blogTabs = $(".bottomContainer .contents #nav3 .panel-tabs .tab");
-    var $blogPreview = $(".bottomContainer #nav3 .blogContainer .blogFrame .preview");
-    var $portPreview = $(".bottomContainer #nav2 .portalContainer .frame .preview");
+    var $tabs = $(".panels .panel-tabs .tab");
+    var $linkers = $(".kontainer .frame .linker").children();
 
-    // 2. If one is clicked on:
-    $all_nav.on("click", function () {
-        // show its data-id content in the contents page
-        var $data_id = $(this).attr("data-id");
-        var $data_content = $("#" + $data_id);
-        var $content_siblings = $data_content.siblings(".content");
+    // Function to handle tab switching
+    function switchTab($tab) {
+        var $kontainer = $tab.closest("article").find(".kontainer");
+        var $frames = $kontainer.find(".frame");
+        var $activeTab = $tab.closest("article").find(".active-tab")
 
-        // hide the content of other siblings with animation
-        $content_siblings.filter(":visible").fadeOut(500, function () {
-            $(this).addClass("hidden");
-            // Show the target class
-            $data_content.fadeIn(100).removeClass("hidden");
+        // Remove the active class from all tabs
+        $activeTab.removeClass("active-tab");
 
-            // retrieve the data-class of the active panel-tab
-            var $activeClass = $data_content.find(".panels .panel-tabs .active-tab").attr("data-class");
-            if ($data_id === "nav2") {
-                // show only blog frame of that activeClass
-                $(".portalContainer .frame").not("." + $activeClass).toggle();
-            } else if ($data_id === "nav3") {
-                // show only blog frame of that activeClass
-                $(".blogContainer .blogFrame").not("." + $activeClass).toggle();
-            };
-        });
+        // Add the active class to the clicked tab
+        $tab.addClass("active-tab");
 
-        // remove the active class from the previous button and put it on this
-        var $active_nav = $(".bottomContainer .navs .active");
-        $active_nav.removeClass("active");
-        $(this).addClass("active");
-        var $title_content = $(this).text();
-        $title.html($title_content);
-    });
+        // Get the class name from the clicked tab
+        var dataClass = $tab.attr("data-class");
 
+        // Hide all frames and show only the frames with the corresponding class
+        $frames.hide();
+        $kontainer.find('.' + dataClass).show();
+    }
+
+    // On tab click:
     $tabs.on("click", function () {
-        // Strip the active tab from the current tab
-        $(".bottomContainer .contents #nav2 .panel-tabs .active-tab").removeClass("active-tab");
-
-        // Make the current button the active tab
-        $(this).addClass("active-tab");
-
-        // Get the class name of the target content and reveal it.
-        var $data_class = $(this).attr("data-class");
-        $(".bottomContainer .contents #nav2 .portalContainer .frame").not('.' + $data_class).fadeOut(1, function () {
-            $(this).addClass("hidden");
-
-            $('.' + $data_class).show();
-        });
+        switchTab($(this));
     });
+
+    // On page load:
+    var $activeTab = $(".panels .panel-tabs .tab.active-tab");
+    switchTab($activeTab);
 
     // Linker tooltip config
     $linkers.hover(function () {
@@ -67,87 +42,4 @@ $(document).ready(function () {
         $(this).find('i').slideToggle();
         $(this).find(".toolTipText").slideToggle();
     })
-
-
-    // Blogs Navigation Config
-    $blogTabs.on("click", function () {
-        // Strip the active tab from the current tab
-        $(".bottomContainer .contents #nav3 .panel-tabs .active-tab").removeClass("active-tab");
-
-        // Make the current button the active tab
-        $(this).addClass("active-tab");
-
-        // Get the class name of the target content and reveal it.
-        var $data_class = $(this).attr("data-class");
-        $(".bottomContainer .contents #nav3 .blogContainer .blogFrame").not('.' + $data_class).fadeOut(1, function () {
-            $(this).addClass("hidden");
-
-            $('.' + $data_class).show();
-        });
-    });
-
-    // Blogs titles config
-    $blogPreview.hover(function () {
-        var title = $(this).find(".previewImg").attr("alt");
-        $(this).find(".previewTitle").text(title);
-        $(this).find(".previewImg").toggle();
-        $(this).find(".previewTitle").toggle();
-    }, function () {
-        $(this).find(".previewImg").toggle();
-        $(this).find(".previewTitle").toggle();
-    });
-
-    $portPreview.hover(function () {
-        var title = $(this).find(".previewImg").attr("alt");
-        $(this).find(".previewTitle").text(title);
-        $(this).find(".previewImg").toggle();
-        $(this).find(".previewTitle").toggle();
-    }, function () {
-        $(this).find(".previewImg").toggle();
-        $(this).find(".previewTitle").toggle();
-    });
-
-
-    // Blog Linker tooltip config ///////////////////////////////
-    $blogLinkers.hover(function () {
-        var $toolText = $(this).attr("name");
-        $(this).find(".toolTipText").text($toolText);
-        $(this).find('i').slideToggle();
-        $(this).find(".toolTipText").slideToggle();
-    }, function () {
-        // Remove the tooltip
-        $(this).find('i').slideToggle();
-        $(this).find(".toolTipText").slideToggle();
-    });
-
-    // FAVOURITE config ///////////////////////////////
-    // Set interval
-    var $favourites = $('#nav6');
-    var $favContainer = $favourites.find('.images');
-    var $favouriteImages = $favContainer.find('.favImg')
-    var currentSlide = 1;
-    var width = 400;
-    var pause = 3000;
-    var animationSpeed = 1000;
-    var longAnimationSpeed = 3000;
-    var interval;
-
-    function startSlider() {
-        interval = setInterval(() => {
-            $favContainer.animate({ 'margin-left': '-=' + width }, animationSpeed, () => {
-                currentSlide++;
-                if (currentSlide === $favouriteImages.length - 2) {
-                    currentSlide = 1;
-                    $favContainer.animate({ 'margin-left': '0' }, longAnimationSpeed);
-                }
-            })
-        }, pause);
-    }
-
-    function stopSlider() {
-        clearInterval(interval);
-    }
-
-    $favContainer.on("mouseenter", stopSlider).on("mouseleave", startSlider);
-    startSlider();
 });
