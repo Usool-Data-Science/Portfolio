e486f9adc6c74f8bceca754e546ef231ba15539b
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-10-07 09:44:08 +0100

    Update

--- src/data/posts.jsx
+++ src/data/posts.jsx
@@ -4,7 +4,7 @@ export const POSTS = [
         title: "How to Start a Flask Application: A Comprehensive Guide",
         datetime: "25-Sep-2024",
         link: "https://dev.to/usooldatascience/how-to-start-a-flask-application-a-comprehensive-guide-29e9",
-        thumbnail: "src/Images/nobgflask.png",
+        thumbnail: "public/Images/nobgflask.png",
         headline: "If you're like me, you’ve probably seen a few different approaches to starting a Flask application and wondered which one is the best. Sometimes, you’ll encounter manage.py, other times, you'll see create_app. This can lead to confusion, especially if you're new to Flask development or transitioning from one project to another. In this article, I will walk you through the most common methods used to start a Flask application, breaking them down with clear examples so you can decide what works best for your use case."
     },
     {
@@ -12,7 +12,7 @@ export const POSTS = [
         title: "Mastering Python Object-Oriented Programming (OOP): A Comprehensive Guide with Examples",
         datetime: "16-Sep-2024",
         link: "https://dev.to/usooldatascience/mastering-python-object-oriented-programming-oop-a-comprehensive-guide-with-examples-f5g",
-        thumbnail: "src/Images/python1.png",
+        thumbnail: "public/Images/python1.png",
         headline: "Object-Oriented Programming (OOP) is one of the most popular programming paradigms used in modern software development. It allows you to model real-world entities using classes and objects, making code reusable, modular, and scalable. In this blog post, we will explore Python's OOP concepts from basic to advanced, using a single use-case example: building an inventory management system for an online store."
     },
     {
@@ -20,7 +20,7 @@ export const POSTS = [
         title: "Mastering Closures and Decorators in Python: From Basics to Advanced",
         datetime: "16-Sep-2024",
         link: "https://dev.to/usooldatascience/mastering-closures-and-decorators-in-python-from-basics-to-advanced-16o",
-        thumbnail: "src/Images/python2.webp",
+        thumbnail: "public/Images/python2.webp",
         headline: "Closures and decorators are powerful features in Python that allow you to write more flexible and reusable code. Understanding these concepts will take your Python skills to the next level, allowing you to handle more complex scenarios like logging, access control, and memoization with ease."
     },
     {
@@ -28,7 +28,7 @@ export const POSTS = [
         title: "Optimizing Python Code Using cProfile and PyPy module: A Complete Guide",
         datetime: "16-Sep-2024",
         link: "https://dev.to/usooldatascience/optimizing-python-code-using-cprofile-and-pypy-module-a-complete-guide-4779",
-        thumbnail: "src/Images/python3.webp",
+        thumbnail: "public/Images/python3.webp",
         headline: "Closures and decorators are powerful features in Python that allow you to write more flexible and reusable code. Understanding these concepts will take your Python skills to the next level, allowing you to handle more complex scenarios like logging, access control, and memoization with ease."
     },
     {
@@ -36,7 +36,7 @@ export const POSTS = [
         title: "Understanding UUIDs: A Backend Engineer’s Guide for Junior Developers",
         datetime: "15-Sep-2024",
         link: "https://dev.to/usooldatascience/understanding-uuids-a-backend-engineers-guide-for-junior-developers-5075",
-        thumbnail: "src/Images/lock.webp",
+        thumbnail: "public/Images/lock.webp",
         headline: "As backend engineers, we're often tasked with building systems that can scale and handle numerous resources, users, and entities, each needing unique identification. In many cases, using sequential IDs (e.g., 1, 2, 3) seems like a straightforward solution, but this can quickly become problematic as your application grows and scales across distributed systems. This is where UUIDs (Universally Unique Identifiers) come in."
     },
     {
@@ -44,7 +44,7 @@ export const POSTS = [
         title: "Mastering Command-Line Interfaces (CLI) in Python: A Comprehensive Guide",
         datetime: "15-Sep-2024",
         link: "https://dev.to/usooldatascience/mastering-command-line-interfaces-cli-in-python-a-comprehensive-guide-10bc",
-        thumbnail: "src/Images/python4.webp",
+        thumbnail: "public/Images/python4.webp",
         headline: "Python is widely known for its versatility and ease of use, especially when building Command-Line Interface (CLI) applications. Whether you want to automate mundane tasks, build developer tools, or create flexible scripts, Python’s rich ecosystem offers various libraries to handle CLI efficiently. In this blog post, we'll dive deep into working with CLIs in Python, covering the following."
     },
     {
@@ -52,7 +52,7 @@ export const POSTS = [
         title: "Mastering Python’s argparse: A Comprehensive Guide for Beginners",
         datetime: "15-Sep-2024",
         link: "https://dev.to/usooldatascience/mastering-pythons-argparse-a-comprehensive-guide-for-beginners-48fn",
-        thumbnail: "src/Images/python5.png",
+        thumbnail: "public/Images/python5.png",
         headline: "Python’s argparse module is a powerful tool for building user-friendly command-line interfaces. Whether you're developing simple scripts or complex applications, knowing how to use argparse effectively can significantly enhance the usability of your programs. In this post, I'll walk you through everything you need to know to master argparse—from basic argument parsing to advanced features and best practices."
     },
     {
@@ -60,7 +60,7 @@ export const POSTS = [
         title: "Writing Great GitHub Issues for Open Source Projects: A Newbie's Guide",
         datetime: "14-Sep-2024",
         link: "https://dev.to/usooldatascience/understanding-threading-and-multiprocessing-in-python-a-comprehensive-guide-48i",
-        thumbnail: "src/Images/github.png",
+        thumbnail: "public/Images/github.png",
         headline: "Contributing to open source projects on GitHub can seem intimidating at first. One of the easiest and most impactful ways to contribute is by creating well-structured issues. Whether you’ve found a bug, need clarification on a feature, or have a suggestion for improvement, a well-written GitHub issue can be the first step toward a successful contribution. In this guide, I'll walk you through how to create issues that will grab attention, get feedback faster, and set you apart in the open-source community. Plus, I'll show you how to use some Markdown tricks to make your issues stand out!."
     },
     {
@@ -68,7 +68,7 @@ export const POSTS = [
         title: "Understanding Threading and Multiprocessing in Python: A Comprehensive Guide",
         datetime: "12-Sep-2024",
         link: "https://dev.to/usooldatascience/mastering-pythons-argparse-a-comprehensive-guide-for-beginners-48fn",
-        thumbnail: "src/Images/multiprocessor.jpg",
+        thumbnail: "public/Images/multiprocessor.jpg",
         headline: "In Python, the concepts of threading and multiprocessing are often discussed when optimizing applications for performance, especially when they involve concurrent or parallel execution. Despite the overlap in terminology, these two approaches are fundamentally different. This blog will help clarify the confusion around threading and multiprocessing, explain when to use each, and provide relevant examples for each concept."
     },
     {
@@ -76,7 +76,7 @@ export const POSTS = [
         title: "A Quick Guide to the Python multiprocessing Module with Examples",
         datetime: "12-Sep-2024",
         link: "https://dev.to/usooldatascience/a-quick-guide-to-the-python-multiprocessing-module-with-examples-4hld",
-        thumbnail: "src/Images/multiprocessor2.jpg",
+        thumbnail: "public/Images/multiprocessor2.jpg",
         headline: "The multiprocessing module in Python allows you to create and manage processes, enabling you to take full advantage of multiple processors on a machine. It helps you achieve parallel execution by using separate memory spaces for each process, unlike threading where threads share the same memory space. Here's a list of commonly used classes and methods in the multiprocessing module with brief examples.."
     },
 ]
