da731d41c9451d6da23ce98aad06c858e0dd4ea7
Usool-Data-Science <adeshinaibrahim10@gmail.com>
2024-10-07 10:40:21 +0100

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// export default defineConfig(({ mode }) => {
-//   return {
-//     base: mode === 'production' ? '/portfolio/' : './',  // Use 'mode' from the config context
-//     plugins: [react()],
-//   }
-// })
-
-// // https://vitejs.dev/config/
-export default defineConfig({
-  // base: "./",
-  base: "/portfolio/",
-  plugins: [react()],
-  server: {
-    port: 3000,
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === 'production' ? '/portfolio/' : './',  // Use 'mode' from the config context
+    plugins: [react()],
   }
 })
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: "./",
+//   // base: "/portfolio/",
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   }
+// })
